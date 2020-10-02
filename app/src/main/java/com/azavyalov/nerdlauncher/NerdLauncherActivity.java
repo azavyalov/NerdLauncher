74634dd957e1cf3d3c41adffdbb456aae14ee80a
azavyalov <alexeysorsa@gmail.com>
2020-10-03 02:08:46 +0300

    Init repo

---
+++ app/src/main/java/com/azavyalov/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,14 @@
+package com.azavyalov.nerdlauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NerdLauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
